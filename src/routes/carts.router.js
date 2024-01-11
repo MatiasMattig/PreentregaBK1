d149192218e0ad6158ba44f4c8967f89d1442c63
Matias Mattig <matu150503@gmail.com>
2024-01-10 22:22:08 -0300

    Primer commit

---
+++ src/routes/carts.router.js
@@ -0,0 +1,17 @@
+const express = require("express"); 
+const router = express.Router(); 
+
+const ProductManager = require("../controllers/product-manager.js"); 
+const productManager = new ProductManager("./src/models/carrito.json");  
+
+router.get("/carts", async (req, res) => {
+    try {
+        const carts = await productManager.getProducts(); 
+        res.json(carts)
+    } catch (error) {
+        console.error("Error al obtener el carrito", error); 
+        res.json({error: "Error del servidor"});
+    }
+})
+
+module.exports = router; 
